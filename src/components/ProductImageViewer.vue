a493348c1cde0217daa72ca13b26eeb590e45952
Sankalpa Senevirathne <dev.sankalpa97@gmail.com>
2024-02-12 00:56:08 +0000

    Fixed image zoom lens bugs

--- src/components/ProductImageViewer.vue
+++ src/components/ProductImageViewer.vue
@@ -1,28 +1,23 @@
 <template>
   <div class="product-images-container relative">
-    <div class="standard-image-container" 
-      @mouseover="showNet"
-      @mouseout="hideNet"
-      @mousemove="updateNetPosition"
-    >
-      <!-- Standard Image -->
+    
+    <!-- Product Image -->
+    <div class="standard-image-container">
       <img
-        :src="standardImagePath(product.images[this.mainImageIndex])"
-        :alt="product.title"
+        id="myimage"
+        :src="standardImagePath(product.images[mainImageIndex])"
         class="product-standard-image"
-        ref="standardImage"
+        :alt="product.title"
+        ref="myImageRef"
+        @mouseover="handleMouseOver"
+        @mouseout="handleMouseOut"
       />
-
-       <!-- Net Overlay -->
-       <div v-if="netVisible" class="net-overlay" :style="netOverlayStyle"></div>
-
     </div>
 
-    <!-- Zoomed Image -->
-    <div v-show="zoomedImageVisible" class="zoomed-image-container">
-      <img :src="largeImagePath(product.images[this.mainImageIndex])" :alt="product.title" class="zoomed-image" />
+    <!-- Product Image Zoom -->
+    <div ref="myResultRef" class="img-zoom-result" :style="{ backgroundImage: 'url(' + largeImagePath(product.images[mainImageIndex]) + ')' }">
     </div>
-
+     
     <!-- Thumbnails -->
     <div class="thumbnail-container">
       <img
@@ -34,10 +29,12 @@
         class="thumbnail"
       />
     </div>
+
   </div>
 </template>
 
 <script>
+
 export default {
   props: {
     product: {
@@ -49,24 +46,16 @@ export default {
     return {
       mainImageIndex: 0,
       zoomedImageVisible: false,
-      netVisible: false,
-      netWidth: 150,
-      netHeight: 100,
-      mousePosition: { x: 0, y: 0 },
+      lens: null,
+      cx: 0,
+      cy: 0,
     };
   },
   computed: {
-    netOverlayStyle() {
-      const mouseX = this.mousePosition.x - this.netWidth;
-      const mouseY = this.mousePosition.y - this.netHeight;
-
-      return {
-        width: `${this.netWidth}px`,
-        height: `${this.netHeight}px`,
-        left: `${mouseX}px`,
-        top: `${mouseY}px`,
-      };
-    },
+  },
+  mounted() {
+    this.createLens();
+    this.imageZoom();
   },
   methods: {
     standardImagePath(image) {
@@ -78,39 +67,97 @@ export default {
     largeImagePath(image) {
       return require(`../assets/images/large/${image.zoomed}`);
     },
-    changeMainImage(index) {
-      this.mainImageIndex = index;
-      this.hideZoomedImage();
-    },
-    showZoomedImage() {
+    handleMouseOver() {
       this.zoomedImageVisible = true;
+      this.imageZoom();
     },
-    hideZoomedImage() {
+
+    handleMouseOut() {
       this.zoomedImageVisible = false;
     },
-    showNet() {
-      this.netVisible = true;
+    changeMainImage(index) {
+      this.mainImageIndex = index;
+      this.imageZoom();
     },
-    hideNet() {
-      this.netVisible = false;
+    createLens() {
+      // Create lens: 
+      this.lens = document.createElement('div');
+      this.lens.setAttribute('class', 'img-zoom-lens');
+      // Insert lens:
+      this.$refs.myImageRef.parentElement.insertBefore(this.lens, this.$refs.myImageRef);
     },
-    updateNetPosition(event) {
-      this.mousePosition = { x: event.clientX - this.netWidth, y: event.clientY - this.netHeight };
+    imageZoom() {
+      console.log('Image zooming');
+      const img = this.$refs.myImageRef;
+      const result = this.$refs.myResultRef;
+      
+      /* Calculate the ratio between result DIV and lens: */
+      this.cx = result.offsetWidth / this.lens.offsetWidth;
+      this.cy = result.offsetHeight / this.lens.offsetHeight;
 
-      if (this.netVisible) {
-        this.hideNet();
-        this.showNet();
+    
+      /* Set background properties for the result DIV */
+      result.style.backgroundSize = (img.width * this.cx) + 'px ' + (img.height * this.cy) + 'px';
+
+      /* Execute a function when someone moves the cursor over the image, or the lens: */
+      this.lens.addEventListener('mousemove', this.moveLens);
+      img.addEventListener('mousemove', this.moveLens);
+      /* And also for touch screens: */
+      this.lens.addEventListener('touchmove', this.moveLens);
+      img.addEventListener('touchmove', this.moveLens);
+    },
+    moveLens(e) {
+      const pos = this.getCursorPos(e);
+      const x = pos.x - (this.lens.offsetWidth / 2);
+      const y = pos.y - (this.lens.offsetHeight / 2);
+      
+      /* Prevent the lens from being positioned outside the image: */
+      if (x > this.$refs.myImageRef.width - this.lens.offsetWidth) {
+        this.x = this.$refs.myImageRef.width - this.lens.offsetWidth;
       }
+      if (x < 0) {
+        this.x = 0;
+      }
+      if (y > this.$refs.myImageRef.height - this.lens.offsetHeight) {
+        this.y = this.$refs.myImageRef.height - this.lens.offsetHeight;
+      }
+      if (y < 0) {
+        this.y = 0;
+      }
+
+      const maxX = this.$refs.myImageRef.width - this.lens.offsetWidth;
+      const maxY = this.$refs.myImageRef.height - this.lens.offsetHeight;
+
+      /* Set the position of the lens: */
+      this.lens.style.left = `${Math.min(Math.max(x, 0), maxX)}px`;
+      this.lens.style.top = `${Math.min(Math.max(y, 0), maxY)}px`;
+      console.log('Move Lens' +'x:'+ x +',y:'+ y);
+
+      /* Display what the lens "sees": */
+      this.$refs.myResultRef.style.backgroundPosition = '-' + (x * this.cx) + 'px -' + (y * this.cy) + 'px';
+
+    },
+    getCursorPos(e) {
+      console.log('Get Cursor');
+      const a = this.$refs.myImageRef.getBoundingClientRect();
+      const x = e.pageX  - a.left - window.scrollX;
+      const y = e.pageY - a.top - window.scrollY;
+      
+      return { x, y };
     },
   },
 };
 </script>
 
-<style scoped>
+<style>
 .product-images-container {
   @apply w-1/2 flex flex-col items-center justify-center bg-white p-12 gap-12;
 }
 
+
+.standard-image-container {
+ @apply relative;
+}
 .product-standard-image {
   @apply h-96 w-96 object-contain;
 }
@@ -126,31 +173,20 @@ export default {
 .thumbnail:hover {
   @apply opacity-75 border-solid rounded-md border-xblue border-2;
 }
-.zoomed-image-container {
-  position: absolute;
-  width: 300px;
-  height: 200px;
-  overflow: hidden;
-  display: none;
-}
 
-.zoomed-image {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
 
-.standard-image-container {
-  position: relative;
+.standard-image-container:hover + .img-zoom-result {
+  @apply block;
 }
 
 
-.net-overlay {
-  position: absolute;
-  width: 100px;
-  height: 100px;
-  border: 2px solid red;
-  pointer-events: none;
-  cursor: crosshair;
+.img-zoom-lens {
+  @apply absolute border-solid border border-gray-300 w-24 h-24 bg-white opacity-70;
 }
+
+.img-zoom-result {
+  @apply hidden absolute border-solid border border-gray-300 w-full h-full left-full top-0 z-50 bg-white;
+  background-repeat: no-repeat;
+}
+
 </style>
